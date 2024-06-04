78d2ceabb90871e0e5af019ba68ae7786111424b
rahuldeshpandesoftwaredev <rahuldeshpandesoftwaredev@gmail.com>
2024-06-03 21:30:36 -0400

    Changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home(){
     return(
         <div>
-            <h1>This is the home page.</h1>
+            <h1>This has been deployed from Netlify.</h1>
             <Link to="/about">Go to About by clicking me.</Link>
         </div>
     )
